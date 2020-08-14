529c6c30186579f2a8ef1cd33abc92f5129ac6a1
Jishant Singh <jishantiitkanpur@gmail.com>
2020-08-14 13:16:35 +0530

    afdg

--- src/FileSystem.java
+++ src/FileSystem.java
@@ -1,33 +1,30 @@
-import com.sun.corba.se.impl.encoding.CDRInputObject;
-
-import java.util.*;
-import java.util.stream.Stream;
+import java.util.ArrayList;
 
 public class FileSystem {
     FSDirectory CDrive;
+
     FSObject getObjectOnPath(String path) {
         String[] dirP = path.substring(4).split("\\\\");
         FSObject obj = this.CDrive;
         int n = dirP.length;
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             String s = dirP[i];
-            if(s == null) continue;
+            if (s == null) continue;
             FSDirectory dir = (FSDirectory) obj;
             boolean found = false;
-            for(FSObject child: dir.contents){
-                if(child.name.equals(s)){
+            for (FSObject child : dir.contents) {
+                if (child.name.equals(s)) {
                     found = true;
                     obj = child;
                     break;
                 }
             }
-            if(!found){
-                if(i == n-1){
+            if (!found) {
+                if (i == n - 1) {
                     FSFile newFile = new FSFile(s, dir, "");
                     dir.contents.add(newFile);
                     obj = newFile;
-                }
-                else{
+                } else {
                     FSDirectory newDir = new FSDirectory(s, dir);
                     dir.contents.add(newDir);
                     obj = newDir;
@@ -37,23 +34,34 @@ public class FileSystem {
         return obj;
     }
 
-    public int CreateFile(String path, String data){
+    public int CreateFile(String path, String data) {
         try {
             FSFile file = (FSFile) getObjectOnPath(path);
             file.setData(data);
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             return 1;
         }
         return 0;
     }
-    public int DeleteFile(String filePath){
+
+    public int DeleteFile(String filePath) {
         try {
             FSFile file = (FSFile) getObjectOnPath(filePath);
-            file
+            file.delete();
+            return 0;
+        } catch (Exception e) {
+            return 1;
         }
+    }
 
-
+    private int ReplaceFileContent(String path, String data) {
+        try {
+            FSFile file = (FSFile) getObjectOnPath(path);
+            file.setData(data);
+            return 0;
+        } catch (Exception e) {
+            return 1;
+        }
     }
 
     public static void main(String[] args) {
@@ -67,8 +75,9 @@ public class FileSystem {
             this.name = name;
             this.parent = par;
         }
-        int delete(){
-            if(this.parent == null) return 1;
+
+        int delete() {
+            if (this.parent == null) return 1;
             parent.deleteObj(this);
             return 0;
         }
@@ -82,7 +91,7 @@ public class FileSystem {
             contents = new ArrayList<>();
         }
 
-        void deleteObj(FSObject obj){
+        void deleteObj(FSObject obj) {
             this.contents.remove(obj);
         }
 
@@ -96,7 +105,8 @@ public class FileSystem {
             super(name, par);
             this.data = data;
         }
-        void setData(String data){
+
+        void setData(String data) {
             this.data = data;
         }
 
